97fa6fd658f3df046c11a888dce55f456c0aa983
LaRaye Johnson <lnj2105@gmail.com>
2019-08-23 20:47:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function writeCards(namesArray, eventName) {
   const thankYouArray = [];
   
   for (let i = 0; i < namesArray.length; i++) {
-    thankYouArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    thankYouArray[i] = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
     
   return thankYouArray;
   }
